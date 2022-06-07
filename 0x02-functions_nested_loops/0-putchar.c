05b3a2ef5fe24817623a15329e1d7316d12a23b2
kirubelAtnafu <kiraatnafu@gmail.com>
2022-06-07 13:36:48 -0700

    first push

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,41 +1,21 @@
 #include "main.h"
 
-
-
 /**
-
  * main - program that prints _putchar
-
- * Description: prints '_putchar\n'
-
+ * Description: prints _putchar
  * Return: always 0 (Success)
-
  *
-
  */
 
-
-
 int main(void)
-  
 {
-  
 
-  
 char i[] = "_putchar\n";
-  
 int j;
-  
 
-  
 for (j = 0; j <= 8; j++)
-    
 {
-      
 _putchar(i[j]);
-      
 }
-  
 return (0);
-  
 }
