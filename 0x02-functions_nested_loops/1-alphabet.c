927a2614cb44d731cda7a4506000cd6efb80fe53
kirubelAtnafu <kiraatnafu@gmail.com>
2022-06-07 13:41:24 -0700

    second push

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - program that print alphabet
+ * Description: prints lowwer case alphabet
+ * Return: Always 0.
+ *
+ */
+
+void print_alphabet(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+
