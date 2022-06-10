a60918625a6f2526b57e86a09d21d7f2f368b66d
kirubelAtnafu <kiraatnafu@gmail.com>
2022-06-10 11:31:09 -0700

    debiugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,7 +12,9 @@
 int main(void)
 {
 int i;
+
 printf("Infinite loop incoming :(\n");
+
 i = 0;
 /*
 *while (i < 10)
@@ -21,5 +23,6 @@ i = 0;
 *}
 */
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
