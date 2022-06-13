c7b0d2b8abb06c8740e9f9dc88c40eff7c42bc19
kirubelAtnafu <kiraatnafu@gmail.com>
2022-06-13 12:21:59 -0700

    zero push

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,6 @@
 
 
 /**
- *
  * reset_to_98 - updates the value of n to 98
  * @n: integer pointer to a variable to be updated
  * Return: nothing
@@ -13,5 +12,5 @@
 
 void reset_to_98(int *n)
 {
-*n = 98; 
+*n = 98;
 }
