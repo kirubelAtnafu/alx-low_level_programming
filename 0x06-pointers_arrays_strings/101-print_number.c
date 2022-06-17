64bdd2b1334a31e6c0a1911afe466b74ec273557
kirubelAtnafu <kiraatnafu@gmail.com>
2022-06-17 03:44:35 -0700

    advanced

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,37 +3,26 @@
 
 
 /**
-
  * print_number - Prints an integer.
-
  * @n: The integer to be printed.
-
  */
-
-
 
 void print_number(int n)
   
 {
-  
-  unsigned int num = n;
-  
+	unsigned int num = n;
 
-  
-  if (n < 0)
-    
-    {
-      
-      _putchar('-');
-      
-      num = -num;
-      
-    }
-  
-  if ((num / 10) > 0)
-    
-    print_number(num / 10);
-  
-  _putchar((num % 10) + '0');
-  
+	if (n < 0)
+	{
+		putchar('-');
+		
+		num = -num;
+	}
+	
+	if ((num / 10) > 0)
+ 
+		print_number(num / 10);
+ 
+		_putchar((num % 10) + '0');
 }
+
