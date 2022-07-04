455b0f2537a02ff56c6a88600226c368cdbf271f
kirubelAtnafu <kiraatnafu@gmail.com>
2022-07-04 14:10:03 -0700

    all of it

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * File: 0-object_like_macro.h
+ * Desc: Header file that defines a macro named
+ *IZE as an abbreviation for the token 1024.
+ */
+
+#define SIZE 1024
+#endif
