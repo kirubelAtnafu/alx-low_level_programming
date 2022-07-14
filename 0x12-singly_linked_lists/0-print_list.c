e1766e24538fdfc348527a169758fc5028741312
kirubelAtnafu <kiraatnafu@gmail.com>
2022-07-14 13:06:02 -0700

    one to four

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,10 +6,10 @@
  * @h: pointer t  head of list
  * Return: number of nodes in list as size_t
  */
-
 size_t print_list(const list_t *h)
 {
 unsigned int nodes = 0;
+
 while (h)
 {
 printf("[%u] ", h->len);
@@ -17,7 +17,7 @@ if (h->str == NULL)
 printf("(nil)\n");
 else
 printf("%s\n", h->str);
-h=h->next;
+h = h->next;
 nodes++;
 }
 return (nodes);
